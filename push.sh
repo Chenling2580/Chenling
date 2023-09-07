581e7ad68510a3cbc12151c4ad87b1774900f1aa
Chenling2580 <3199293788@qq.com>
2023-09-07 11:56:04 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-git config--global user.name'Chenling2580'
-git config--global user.email'3199293788@qq.com'
-git config--global user.http.sslverify false
-git add.
-git commit-m'自动推送'
+git config --global user.name 'Chenling2580'
+git config --global user.email '3199293788@qq.com'
+git config --global http.sslverify false
+git add .
+git commit -m '自动推送'
 git push
